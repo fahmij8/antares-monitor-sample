0288afb51289c10628a43680d7b94e468155ddb5
unknown <rootmawar77@gmail.com>
2020-08-31 23:11:08 +0700

    Version 1 - Add Chart data

---
+++ index.php
@@ -0,0 +1,73 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Monitoring Kualitas Air</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href='./materialize/css/materialize.min.css'>
+    <link type="text/css" rel="stylesheet" href='./materialize/css/custom.css'>
+</head>
+<body>
+    <header>
+        <!-- Title Section -->
+        <div class="title blue lighten-3 valign-wrapper z-depth-2">
+            <h3>Monitor Kualitas Air</h3>
+        </div>
+    </header>
+
+    <main>
+        <!-- Display Data Section -->
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m6 l3">
+                    <div class="card-panel">
+                        <img src="./materialize/1.png" class="responsive-image">
+                        <h1>Kekeruhan</h1>
+                        <span class="black-text force-center" id="data1"></span>
+                    </div>
+                </div>
+                <div class="col s12 m6 l3">
+                    <div class="card-panel">
+                        <img src="./materialize/2.png" class="responsive-image">
+                        <h1>pH</h1>
+                        <span class="black-text force-center" id="data2"></span>
+                    </div>
+                </div>
+                <div class="col s12 m6 l3">
+                    <div class="card-panel">
+                        <img src="./materialize/3.png" class="responsive-image">
+                        <h1>Suhu</h1>
+                        <span class="black-text force-center" id="data3"></span>
+                    </div>
+                </div>
+                <div class="col s12 m6 l3">
+                    <div class="card-panel">
+                        <img src="./materialize/4.png" class="responsive-image">
+                        <h1>Kelembaban</h1>
+                        <span class="black-text force-center" id="data4"></span>
+                    </div>
+                </div>
+            </div>
+        
+
+            <!-- Button Update -->
+            <div class="force-center">
+                <a class="waves-effect waves-light btn blue darken-1 pulse" id="upd">Update</a>
+            </div>
+
+            <!-- Chart -->
+            <div class="chart-data">
+                <canvas id="chart"></canvas>
+            </div>
+        </div>
+    </main>
+
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="./materialize/js/materialize.min.js"></script>
+<script src="./materialize/js/custom.js"></script>
+</html>
---
+++ materialize/js/custom.js
@@ -0,0 +1,146 @@
+$(window).on('load', () => {
+    // Inisialisasi variable
+    let data = 0 // Variable untuk fetching data
+
+    let labels = []
+    let history_kekeruhan = []
+    let history_ph = []
+    let history_suhu = []
+    let history_kelembaban = []
+
+    // Variable untuk menampilkan data
+    const data1 = document.getElementById("data1") 
+    const data2 = document.getElementById("data2") 
+    const data3 = document.getElementById("data3") 
+    const data4 = document.getElementById("data4") 
+
+    // Inisialisasi fungsi mengambil data dari API
+    getData = async () => {
+        let makeRequest = new XMLHttpRequest()
+        makeRequest.onload = function() {
+            data = this.responseText        
+        }
+        makeRequest.open("get", "get.php", true)
+        makeRequest.send()
+        data1.innerHTML = "Loading data..."
+        data2.innerHTML = "Loading data..."
+        data3.innerHTML = "Loading data..."
+        data4.innerHTML = "Loading data..."
+    }
+
+    // Menjalankan fungsi get data di awal load page
+    getData();
+
+    // Update HTML dari data API
+    const update = () => {
+        let js_data = JSON.parse(JSON.parse(data))
+        let js_obj_data = JSON.parse(js_data["m2m:cin"].con)
+
+        convertData(js_obj_data)
+
+        data1.innerHTML = js_obj_data["Kekeruhan"]
+        data2.innerHTML = js_obj_data["pH"]
+        data3.innerHTML = js_obj_data["Suhu"]
+        data4.innerHTML = js_obj_data["Kelembaban"]
+    }
+
+    // Inisialisasi fungsi run 
+    const run = () => {
+        setTimeout(() => {
+            getData()
+            update()
+        }, 1000)
+    }
+
+    // Menjalankan fungsi run di awal load page
+    run()
+
+    // Memberikan attribut onclick pada elemen dengan id upd (button update)
+    $("#upd").click(function() {
+        run()
+        M.toast({html: "Data Updated!"})
+    })
+    
+
+    // Chart
+    let convertData = (jsdata) => {
+        let today = new Date();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        
+        if (labels.length > 4){
+            labels.shift()
+            history_kekeruhan.shift()
+            history_ph.shift()
+            history_suhu.shift()
+            history_kelembaban.shift()
+
+            labels.push(time)
+            history_kekeruhan.push(parseInt(jsdata["Kekeruhan"]))
+            history_ph.push(parseInt(jsdata["pH"]))
+            history_suhu.push(parseInt(jsdata["Suhu"]))
+            history_kelembaban.push(parseInt(jsdata["Kelembaban"]))
+        } else {
+            labels.push(time)
+            history_kekeruhan.push(parseInt(jsdata["Kekeruhan"]))
+            history_ph.push(parseInt(jsdata["pH"]))
+            history_suhu.push(parseInt(jsdata["Suhu"]))
+            history_kelembaban.push(parseInt(jsdata["Kelembaban"]))
+        }
+
+
+        let options = {
+          type: 'line',
+          data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Kekeruhan',
+                        data: history_kekeruhan,
+                        borderWidth: 1,
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)'
+                    },
+                    {
+                        label: 'pH',
+                        data: history_ph,
+                        borderWidth: 1,
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)'
+                    },
+                    {
+                        label: 'Suhu',
+                        data: history_suhu,
+                        borderWidth: 1,
+                        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                        borderColor: 'rgba(255, 206, 86, 1)'
+                    },
+                    {
+                        label: 'Kelembaban',
+                        data: history_kelembaban,
+                        borderWidth: 1,
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderColor: 'rgba(75, 192, 192, 1)'
+                    }
+                ]
+              },
+              options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            reverse: false
+                        }
+                    }]
+                },
+                maintainAspectRatio : false,
+                aspectRatio : 1
+            }
+        }
+
+        execChart(options)
+    }
+
+    const execChart = (options) => {
+        let ctx = document.getElementById('chart').getContext('2d');
+        new Chart(ctx, options);
+    }
+})
